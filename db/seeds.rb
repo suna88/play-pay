d3e48d9819f918a5cf4481c5aae1c358ecb7690a
suna88 <alotofthings88@gmail.com>
2018-03-30 23:36:58 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,5 @@
 
 
 #User.create(name:'Master', email:'Kmori@japan.lakeland.edu', password:'Kohei19960704', meta_address:'0xBEDdD2686455DD6cB99a34A055679B92030928d8',admin:1)
-User.create(name:'Master', email:'alotofthings88@gmail.com', password:'Sunami88', meta_address:'0xBEDdD2686455DD6cB99a34A055679B92030928d8',admin:1)
+#User.create(name:'Master', email:'alotofthings88@gmail.com', password:'Sunami88', meta_address:'0xBEDdD2686455DD6cB99a34A055679B92030928d8',admin:1)
+Coin.create(buy_price_per_yen:10000)
