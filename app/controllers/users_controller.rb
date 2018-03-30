f3826c77a72904579dd9f08714159d344ab9ab31
suna88 <alotofthings88@gmail.com>
2018-03-30 19:25:36 +0900

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:show,:editm, :update]
+
   def index
     logger.debug('_______________')
     logger.debug(current_user.admin)
@@ -22,16 +24,34 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-
   end
 
   def show
     @user = current_user
   end
 
+
+  def edit
+
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:email, :name, :password, :meta_address)
+  end
+
+  def set_user
+      @user = User.find(params[:id])
   end
 
 end
