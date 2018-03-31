b95c7faa2475fdc2e71e98a834a75618fe32a0b8
suna88 <alotofthings88@gmail.com>
2018-03-31 23:25:08 +0900

    emerge

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :password, presence:true, length:{minimum:6}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
   has_many :trades
   has_secure_password
 
